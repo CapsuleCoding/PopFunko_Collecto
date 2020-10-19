cf8a3f52ed35ae9a98e0eaa4a1b6fce6e3d87ef8
Capsulecoding <capsulecoding@icloud.com>
2020-10-19 15:13:44 -0700

    sessions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,51 @@
+class Sessions Controller < ApplicationController
+
+    def welcome
+    end
+
+    def new
+        @user = User.new
+    end
+
+    def create 
+
+        if params[:provider] == 'google oauth2'
+            @user = User.create_by_google_omniauth(auth)
+            session[:user_id] = @user_id
+            redirect_to user_path(@user)
+
+        elsif
+            @user = User.find_by(username: params[:user][:username])
+            @user && @user.authenicate(parms[:user][:password])
+            session[:user_id] = @user.user_id
+            redirect_to user_path(@user)
+
+        else
+            flash[:notice] = "One or more fields incorrect, please try again"
+            redirect_to login_path
+        end
+    end
+
+    def destroy
+        session.delete(:user_id)
+        flash[:notice] = "Successfully Logged Out"
+        redirect_to '/' notice: "Successfully Logged Out"
+    end
+
+    def omniauth
+        if @user
+            create_session
+            redirect_to user_path(@user)
+        else
+            redirect_to login_path
+        end
+    end
+
+    private
+
+    def auth
+        request.env['omniauth.auth']
+    end
+end
+
+
