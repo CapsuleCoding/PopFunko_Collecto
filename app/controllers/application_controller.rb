c0be934de551ef5a7e9d2405da4ba0b1705d8b77
Capsulecoding <capsulecoding@icloud.com>
2020-10-19 13:16:32 -0700

    First

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,21 @@
+class ApplicationController < ActionController::Base
+    helper_method :logged_in?
+    helper_method :create_session
+    helper_method :current_user
+
+    def require_login
+        redirect_to login_path unless session[:user_id]
+    end
+
+    def logged_in?
+        session[:user_id] ? true : false
+    end
+
+    def create_session
+        session[:user_id] = @user.user_id
+    end
+
+    def current_user
+        session[:user_id
+    end]
+end
---
+++ app/controllers/pop_funkos_controller.rb
@@ -0,0 +1,60 @@
+class PopFunkosController < ApplicationController
+    before_action :set_pop_funko, only: [:edit, :update, :show, :destroy]
+
+    before_action :require_login
+
+
+    def new 
+        @pop_funko = PopFunko.new
+        @pop_funko.build_category
+    end
+
+    def create
+        @pop_funko = PopFunko.new(pop_funko_params)
+        @pop_funko.user_id = session[:user_id]
+            if @pop_funko.save
+                redirect_to pop_funko_path(@pop_funko)
+            else
+                render :new
+            end
+        end
+    end
+
+    def index
+        @@pop_funko = PopFunko.order_by_store
+    end
+
+    def show
+        @stores = @pop_funko.stores
+    end
+
+    def edit
+    end
+
+    def update
+        if @pop_funko.update(pop_funko_params)
+            redirect_to pop_funko_path
+              flash[:notice] = "Update is now complete!"
+
+        else
+            render :edit
+
+        end
+
+    end
+
+    def destroy
+        @pop_funko.destroy
+        redirect_to pop_funko_path
+        flash[:notice] = "Delete Complete"
+    end
+
+    private
+
+    def pop_funko_params
+        params.require(:pop_funko).permit(:name, :price, :location_id, location_attributes: [:name, :address])
+    end
+
+    def set_pop_funko
+        @pop_funko = PopFunko.find_by(id: params[:id])
+    end
