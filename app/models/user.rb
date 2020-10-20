06467f9e4ea8bee924d9087c67d874ff13b8a3fd
Capsulecoding <capsulecoding@icloud.com>
2020-10-19 23:20:18 -0700

    update

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
-class User < ApllicationRecord
+class User < ApplicationRecord
     has_many :locations
-    has_many :location_pop_funkos, through: :locations, sources: :pop_funko 
+    has_many :locations_pop_funkos, through: :locations, source: :pop_funko 
     has_many :pop_funkos
     has_secure_password
 
     validates :username, uniqueness:true, presence:true
-    validates :password, length: { in: 6..12}
+    validates :password, length: { in: 6..100}
 
     def self.create_by_google_omniauth(auth)
         self.find_or_create_by(username: auth[:info][:email]) do |u|
