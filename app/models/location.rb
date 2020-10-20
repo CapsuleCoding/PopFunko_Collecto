aaf47ce90a25f1fa0113e2e02de3ed68782b53e9
Capsulecoding <capsulecoding@icloud.com>
2020-10-19 23:19:31 -0700

    update

--- app/models/location.rb
+++ app/models/location.rb
@@ -3,6 +3,7 @@ class Location < ApplicationRecord
     belongs_to :user
 
     validates :address, presence: true
+    validates :bought, presence: true
 
     validates :pop_funko, uniqueness: { scope: :user, message:"Already added location"}
     
