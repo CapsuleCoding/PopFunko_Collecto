c79a0dd9dd94fad4a49424be3432188a17a344ba
Capsulecoding <capsulecoding@icloud.com>
2020-10-19 18:45:08 -0700

    update

---
+++ app/models/pop_funko.rb
@@ -0,0 +1,29 @@
+class PopFunk < ApplicationRecord
+    belongs_to :category
+    belongs_to :user
+    has_many :locations, dependent: :destroy
+    has_many :users, through: :locations
+    accepts_nested_attributes_for :category
+
+    validates :name, presenc: true
+    validates :price, presence: true
+    validates :already_exist
+
+    scope :order_by_location, -> {left_joins(:locations).group(:id).order("price desc")}
+
+    def category_attributes(attributes)
+        category = Category.find_or_create_by(attributes) if !name.empty
+    end
+
+    def already_exist
+        if PopFunko.find_by(name: name,price: price)
+            errors.add(:name, 'has already been added for that price')
+        end
+    end
+
+    def name_of_location
+        "#{name} - #{sponsor.name}"
+    end
+    
+
+end
