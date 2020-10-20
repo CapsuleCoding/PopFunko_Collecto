11498df28a337a2eacc50acda5dbd9aba8e9b7bc
Capsulecoding <capsulecoding@icloud.com>
2020-10-19 23:18:04 -0700

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
+  
+  get '/' => 'sessions#welcome'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/auth/:provider/callback' => 'sessions#omniauth'
+  
+  resources :locations, only: [:new, :index, :create]
+
+  resources :pop_funkos do
+    resources :locations, only: [:new, :index]  
+  end 
+
+  resources :categories
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
